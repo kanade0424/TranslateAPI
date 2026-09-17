4794a3655f2971e472b28a97f4f52c2d948ee74f
奏 <kanade.miyachi@gmail.com>
2026-09-17 20:02:50 +0900

    ｓ

--- src/main.rs
+++ src/main.rs
@@ -40,4 +40,7 @@ async fn translate_handler(
 
 async fn status_handler() -> StatusCode {
     let translator = Translator::default();
+    let start_time = std::time::Instant::now();
+    let result = translator.translate("en", "ja", "healthcheck").await;
+    let duration = start_time.elapsed();
 }
