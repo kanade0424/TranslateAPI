6e6239fad39bd7312d8b87d75e5703b50c367a51
奏 <kanade.miyachi@gmail.com>
2026-09-17 20:27:21 +0900

    portの呼び出しから&を削除してuに統一

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ async fn main(){
         .route("/translate", post(translate_handler))
         .route("/status", get(status_handler));
     
-    let addr = SocketAddr::from(([127, 0, 0, 1], &port));
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
 
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
