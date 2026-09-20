0e5ee4f36a8db18e12097fe2344ec11f35c7cdf1
kanade0424 <kanade.miyachi@gmail.com>
2026-09-20 00:41:29 +0000

    正しい関数名に

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use axum::{
 };
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
-use trad::{Translator,Language,languages};
+use trad::{Translator,languages};
 use std::sync::Arc;
 #[derive(Deserialize, Debug)]
 struct TranslateRequest {
